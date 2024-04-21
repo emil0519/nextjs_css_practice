ebe32c0ce76547fdd38f32ee5afc14afd2b43ac6
helloworldFromTW <emillau@nine-yi.com>
2024-04-21 10:35:24 +0000

    feat: header layout & hamburger mechanism

---
+++ app/component/hamburger.tsx
@@ -0,0 +1,17 @@
+export const Hamburger = ({
+  isOpen,
+}: {
+  isOpen: boolean;
+}): React.ReactElement => (
+  <button className="flex flex-col justify-between h-6 w-6">
+    <div
+      className={`h-1 w-full bg-black transition-all duration-200 ${isOpen && "transform translate-y-3 rotate-45"}`}
+    ></div>
+    <div
+      className={`h-1 w-2/3 bg-black transition-all duration-200 ${isOpen ? "opacity-0" : ""}`}
+    ></div>
+    <div
+      className={`h-1 w-full bg-black transition-all duration-200 ${isOpen && "transform -translate-y-2 -rotate-45"}`}
+    ></div>
+  </button>
+);
---
+++ app/component/header.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { useState } from "react";
+import { Hamburger } from "./hamburger";
+
+export const Header = (): React.ReactElement => {
+  const [isOpen, setIsopen] = useState<boolean>(false);
+  return (
+    <nav className="bg-white h-16 border-b border-solid shadow-md w-full">
+      <div className="flex justify-between mt-6 mx-8">
+        <div onClick={() => setIsopen((prevState) => !prevState)}>
+          <Hamburger isOpen={isOpen} />
+        </div>
+        <h1 className="text-2xl font-bold">白頭翁不吃小米</h1>
+        <div>logo</div>
+      </div>
+    </nav>
+  );
+};
